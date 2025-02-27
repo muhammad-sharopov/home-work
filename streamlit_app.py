f18c0cfbc860ad2c15b87ba09146f95cba8fb98f
muhammad-sharopov <82517976+muhammad-sharopov@users.noreply.github.com>
2025-02-27 19:46:33 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -284,8 +284,8 @@ model_dict = {
 
 correlation = features_up_10_unique.corrwith(y).abs()
 top_feature = correlation.nlargest(3)
-top_2_features = top_feature.index[:2] 
-X_2 = data[top_2_features.tolist()]
+top_2_features = top_feature.head(2).index 
+X_2 = data[top_2_features]
 X_train_2, X_test_2, y_train, y_test = train_test_split(X_2, y, test_size=0.3, random_state=42)
 X_train_2_scaled = scaler.fit_transform(X_train_2)
 X_test_2_scaled = scaler.transform(X_test_2)
