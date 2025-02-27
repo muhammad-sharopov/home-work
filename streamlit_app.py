dd5f615f7b25bca25119f1798c0632c2658976a1
muhammad-sharopov <82517976+muhammad-sharopov@users.noreply.github.com>
2025-02-27 18:04:17 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,24 +54,26 @@ if selected_view == "Rows":
     if "show_rows" not in st.session_state:
         st.session_state.show_rows = False
 
+    st.write("Number of rows:", data.shape[0])
+
     if st.button("Show Rows"):
         st.session_state.show_rows = not st.session_state.show_rows 
 
     if st.session_state.show_rows:
-        st.write("Number of rows:", data.shape[0])
-        st.dataframe(data) 
+        st.dataframe(data.head()) 
 
 elif selected_view == "Columns":
     if "show_columns" not in st.session_state:
         st.session_state.show_columns = False
 
+    st.write("Number of columns:", data.shape[1])
+
     if st.button("Show Columns"):
         st.session_state.show_columns = not st.session_state.show_columns 
 
     if st.session_state.show_columns:
-        st.write("Number of columns:", data.shape[1])
         st.write("Column names:")
-        st.write(data.columns) 
+        st.write(data.columns.tolist()) 
 
 selected_column = st.selectbox("Select a column:", data.columns)
 
