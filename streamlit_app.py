bd57bc803cd92ceabef6fbbfc042d224c24b0abe
muhammad-sharopov <82517976+muhammad-sharopov@users.noreply.github.com>
2025-02-27 19:31:55 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -281,7 +281,9 @@ model_dict = {
     "Decision Tree": decision_tree_2,
     "KNN": knn
 }
-top_feature = correlations.nlargest(3)
+
+correlation = features_up_10_unique.corrwith(y).abs()
+top_feature = correlation.nlargest(3)
 top_2_features = top_feature.head(2).index.tolist()
 X_2 = data[top_2_features]
 X_train_2, X_test_2, y_train, y_test = train_test_split(X_2, y, test_size=0.3, random_state=42)
